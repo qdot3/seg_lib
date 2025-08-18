0f5546508334e9bd76bd8fea9e487ce5b67d7b67
qdot3 <quantum.engine.55g@gmail.com>
2025-08-18 17:18:35 +0900

    modify

--- src/provider/min.rs
+++ src/provider/min.rs
@@ -1,11 +1,11 @@
 use crate::traits::{Query, Update};
 
 #[derive(Debug)]
-pub struct MinProvider;
+pub struct Min;
 
 macro_rules! impl_query {
     ($( $t:ty )*) => {$(
-        impl Query<$t> for MinProvider {
+        impl Query<$t> for Min {
             fn identity() -> $t {
                 <$t>::MAX
             }
@@ -21,7 +21,7 @@ impl_query!( u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize );
 
 macro_rules! impl_query_float {
     ($( $t:ty )*) => {$(
-        impl Query<$t> for MinProvider {
+        impl Query<$t> for Min {
             fn identity() -> $t {
                 <$t>::INFINITY
             }
@@ -37,7 +37,7 @@ impl_query_float!( f32 f64 );
 
 macro_rules! impl_update {
     ($( $t:ty )*) => {$(
-        impl Update<$t> for MinProvider {
+        impl Update<$t> for Min {
             type Set = $t;
 
             fn identity() -> $t {
@@ -59,7 +59,7 @@ impl_update!( u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize );
 
 macro_rules! impl_update_float {
     ($( $t:ty )*) => {$(
-        impl Update<$t> for MinProvider {
+        impl Update<$t> for Min {
             type Set = $t;
 
             fn identity() -> $t {
