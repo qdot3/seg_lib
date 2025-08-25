25dabd106584c16e1579f9cf3862666bc9022efc
qdot3 <quantum.engine.55g@gmail.com>
2025-08-25 21:36:40 +0900

    misc

--- src/lib.rs
+++ src/lib.rs
@@ -12,59 +12,6 @@ pub use lazy::LazySegmentTree;
 mod beats;
 pub use beats::SegmentTreeBeats;
 
-/// Predefined operations on segment tree variants.
-///
-/// # Custom provider
-///
-/// ```ignore
-/// pub struct Custom<T>(PhantomData<T>);
-///
-/// impl<T> Monoid for Custom<T> { /* ---- */ }
-/// ```
-///
-/// # TODO
-///
-/// If `negative_trait_impl` is stabilized, rewrite trait bounds as follows:
-///
-/// ```ignore
-/// impl Monoid for Add<T>
-/// where
-///     T: !Copy
-///     for<'a> &'a T: std::ops::Add<Output =T>
-/// { /* methods */ }
-///
-/// impl Monoid for Add<T>
-/// where
-///     T: Copy + std::ops::Add<Output = T>
-/// { /* methods */ }
-/// ```
-pub mod provider {
-    mod add;
-    pub use add::Add;
-
-    mod mul;
-    pub use mul::Mul;
-
-    mod bit_and;
-    pub use bit_and::BitAnd;
-
-    mod bit_or;
-    pub use bit_or::BitOr;
-
-    mod bit_xor;
-    pub use bit_xor::BitXor;
-
-    mod max;
-    pub use max::Max;
-
-    mod min;
-    pub use min::Min;
-
-    mod assign;
-    pub use assign::Assign;
-
-    mod affine;
-    pub use affine::Affine;
-}
+pub mod provider;
 
 pub mod traits;
---
+++ src/provider/mod.rs
@@ -0,0 +1,52 @@
+//! Predefined operations on segment tree variants.
+//!
+//! # Custom provider
+//!
+//! ```ignore
+//! pub struct Custom<T>(PhantomData<T>);
+//!
+//! impl<T> Monoid for Custom<T> { /* ---- */ }
+//! ```
+//!
+//! # TODO
+//!
+//! If `negative_trait_impl` is stabilized, rewrite trait bounds as follows:
+//!
+//! ```ignore
+//! impl Monoid for Add<T>
+//! where
+//!     T: !Copy
+//!     for<'a> &'a T: std::ops::Add<Output =T>
+//! { /* methods */ }
+//!
+//! impl Monoid for Add<T>
+//! where
+//!     T: Copy + std::ops::Add<Output = T>
+//! { /* methods */ }
+//! ```
+mod add;
+pub use add::Add;
+
+mod mul;
+pub use mul::Mul;
+
+mod bit_and;
+pub use bit_and::BitAnd;
+
+mod bit_or;
+pub use bit_or::BitOr;
+
+mod bit_xor;
+pub use bit_xor::BitXor;
+
+mod max;
+pub use max::Max;
+
+mod min;
+pub use min::Min;
+
+mod assign;
+pub use assign::Assign;
+
+mod affine;
+pub use affine::Affine;
